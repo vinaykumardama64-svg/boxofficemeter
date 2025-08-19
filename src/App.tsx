aec92bf865f6d40e2af2b5aa6f67c060f3b9b68e
Vinay Kumar <vinaykumardama64@gmail.com>
2025-08-20 01:00:15 +0530

    modiefied app.ltss

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,3 @@
-<<<<<<< HEAD
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
-=======
 import React from "react";
 import { useEffect, useState } from "react";
 import Papa from "papaparse";
@@ -147,4 +110,3 @@ function App() {
 }
 
 export default App;
->>>>>>> 00c603de9d4ec49b41c1847249c654ffa54f68ef
